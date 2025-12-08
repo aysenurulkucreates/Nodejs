5f0e397d76bd2a94d314352c007e5f73e8bab8d0
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-08 11:41:24 +0300

    Deleting products

--- SQLPart/3_mongoDb/controllers/admin.js
+++ SQLPart/3_mongoDb/controllers/admin.js
@@ -90,14 +90,10 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-/*
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findByPk(prodId)
-    .then(product => {
-        return product.destroy();
-    })
-    .then(result => {
+    Product.deleteById(prodId)
+    .then(() => {
         console.log('DESTROYED PRODUCT!');
         res.redirect('/admin/products');
     })
@@ -105,4 +101,3 @@ exports.postDeleteProduct = (req, res, next) => {
         console.log(err);
     });
 };
-*/
