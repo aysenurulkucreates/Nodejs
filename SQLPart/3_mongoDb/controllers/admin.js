46df50ec15005428708bc27088dd901aca84070e
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-08 11:21:02 +0300

    Finishing the update product code

--- SQLPart/3_mongoDb/controllers/admin.js
+++ SQLPart/3_mongoDb/controllers/admin.js
@@ -57,15 +57,16 @@ exports.postEditProduct = (req, res, next) => {
     const updatedImageUrl = req.body.imageUrl;
     const updatedPrice = req.body.price;
     const updatedDesc = req.body.description;
-    Product.findById(prodId)
-    .then(product => {
-        product.title = updatedTitle;
-        product.imageUrl = updatedImageUrl;
-        product.price = updatedPrice;
-        product.description = updatedDesc;
-        return product.save();
-    })
-    .then(result => {
+    const product = new Product(
+        updatedTitle,
+        updatedImageUrl,
+        updatedPrice,
+        updatedDesc,
+        prodId
+    );
+        product
+        .save()
+        .then(result => {
         console.log('UPDATED PRODUCT!');
         res.redirect('/admin/products');
     })
