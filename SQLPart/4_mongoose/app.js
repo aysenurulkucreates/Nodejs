1e009a55a78d5f8206787db056b1d8dab990909b
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-10 09:43:19 +0300

    Connecting to the MongoDB Server with Mongoose

---
+++ SQLPart/4_mongoose/app.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findById('69369dd6b5bdc738b1e83788')
+    .then(user => {
+        req.user = new User(user.name, user.email, user.cart, user._id);
+        next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404Page);
+
+mongoose 
+.connect(process.env.MONGODB_URI)
+.then(result => {
+    console.log('Connected');
+    app.listen(3000);
+})
+.catch(err => {
+    console.log(err);
+})
+
+
