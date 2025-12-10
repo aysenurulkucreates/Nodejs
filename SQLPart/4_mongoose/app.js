0c847742c1932e85290dec28b26bad4050083809
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-10 10:17:21 +0300

    Saving data through mongoose

--- SQLPart/4_mongoose/app.js
+++ SQLPart/4_mongoose/app.js
@@ -1,11 +1,12 @@
 const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+//const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +19,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     User.findById('69369dd6b5bdc738b1e83788')
     .then(user => {
         req.user = new User(user.name, user.email, user.cart, user._id);
         next();
     })
     .catch(err => console.log(err));
-});
+});*/
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
