75ded44f7c02c000033faa71ba039b504da7b92f
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-12 15:17:38 +0300

    The current project status with cookies

---
+++ SessionsCookies/1_introduction/app.js
@@ -0,0 +1,57 @@
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findOne()
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404Page);
+
+mongoose 
+.connect(process.env.MONGODB_URI)
+.then(result => {
+    User.findOne().then(user => {
+    if(!user) {
+       const user = new User ({
+        name: 'Ayse',
+        email: 'ayse@test.com',
+        cart: {
+            items:[]
+        }
+    });
+    user.save();
+    }
+    })
+    app.listen(3000);
+})
+.catch(err => {
+    console.log(err);
+})
+
+
