5dafb729ddac5d2e797bb9aa43fe532193d89c73
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-13 15:14:32 +0300

    Using MongoDB to store sessions

--- SessionsCookies/1_introduction/app.js
+++ SessionsCookies/1_introduction/app.js
@@ -4,11 +4,19 @@ require('dotenv').config({ path: path.join(__dirname, '../.env') });
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = process.env.MONGODB_URI;
+
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +27,12 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+}));
 
 app.use((req, res, next) => {
     User.findOne()
@@ -36,7 +50,7 @@ app.use(authRoutes);
 app.use(errorController.get404Page);
 
 mongoose 
-.connect(process.env.MONGODB_URI)
+.connect(MONGODB_URI)
 .then(result => {
     User.findOne().then(user => {
     if(!user) {
