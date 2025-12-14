8eaa9a3233c245941efacc01fac9679289bccb9a
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-14 14:14:13 +0300

    update current project wiğth authentication

---
+++ SessionsCookies/2_addingAuthentication/controllers/auth.js
@@ -0,0 +1,41 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+   console.log(req.session.isLoggedIn);
+    res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+   });
+};
+
+exports.getSignup = (req, res, next) => {
+   console.log(req.session.isLoggedIn);
+    res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+   });
+};
+
+exports.postLogin = (req, res, next) => {
+     User.findById('693bb5671243744b5687238e')
+    .then(user => {
+        req.session.user = user;
+        req.session.isLoggedIn = true;
+        req.session.save(err => {
+            console.log(err);
+            res.redirect('/');
+        });
+    })
+    .catch(err => console.log(err));
+};
+
+//exports.postSignup = (req, res, next) => {};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        console.log(err);
+        res.redirect('/');
+    });
+};
