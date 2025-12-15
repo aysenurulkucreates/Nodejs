3e3f13bacb6b20e1f01add539af061cf879d115b
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-15 18:22:02 +0300

    Adding the signin functionality

--- SessionsCookies/2_addingAuthentication/controllers/auth.js
+++ SessionsCookies/2_addingAuthentication/controllers/auth.js
@@ -21,15 +21,27 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-     User.findById('693bb5671243744b5687238e')
+    const email = req.body.email;
+    const password = req.body.password;
+     User.findOne({email: email})
     .then(user => {
-        req.session.user = user;
-        req.session.isLoggedIn = true;
-        req.session.save(err => {
-            console.log(err);
-            res.redirect('/');
+        if (!user) {
+            return res.redirect('/login');
+        }
+        bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+            if (doMatch) {
+             req.session.user = user;
+             req.session.isLoggedIn = true;
+             return req.session.save(err => {
+             console.log(err);
+             res.redirect('/');
+            });
+            }
+            res.redirect('/login');
         });
-    })
+        })
     .catch(err => console.log(err));
 };
 
