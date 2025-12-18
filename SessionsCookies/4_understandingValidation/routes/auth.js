7f407686ec288c7c6ff15d920bc4bdfdb85c6c35
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-18 18:57:56 +0300

    More validators

--- SessionsCookies/4_understandingValidation/routes/auth.js
+++ SessionsCookies/4_understandingValidation/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -12,7 +12,8 @@ router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
 
 router.post('/signup',
-     check('email')
+[ 
+    check('email')
     .isEmail()
     .withMessage('Please enter a valid email.')
     .custom((value, {req}) => {
@@ -21,6 +22,12 @@ router.post('/signup',
         }
         return true;
     }),
+    body(
+        'password',
+        'Please enter a password with only numbers and text and at least 5 characters.'
+    ).isLength({ min: 5 })
+    .isAlphanumeric()
+],
     authController.postSignup);
 
 router.post('/logout', authController.postLogout);
