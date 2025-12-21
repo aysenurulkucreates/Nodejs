224f4fbf208ea41263f0bbfcf860c5d35fbd3ca1
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-21 12:59:01 +0300

    Setting file type headers

--- SessionsCookies/6_fileUploadAndDownland/controllers/shop.js
+++ SessionsCookies/6_fileUploadAndDownland/controllers/shop.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const PDFDocument = require('pdfkit');
 
 const Product = require('../models/product');
 const Order = require('../models/order');
@@ -154,10 +155,13 @@ exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
   const invoiceName = 'invoice-' + orderId + '.pdf';
   const invoicePath = path.join(__dirname, '..', 'data', 'invoices', invoiceName);
+  const pdfDoc = new PDFDocument();
   fs.readFile(invoicePath, (err, data) => {
     if (err) {
       return next(err);
     }
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
     res.send(data);
 });
 };
