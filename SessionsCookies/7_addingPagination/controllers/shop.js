505b6b90bc303eee9592c8b6c440acccbc8d65b7
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-22 09:12:58 +0300

    Retreiving a chunk of data

--- SessionsCookies/7_addingPagination/controllers/shop.js
+++ SessionsCookies/7_addingPagination/controllers/shop.js
@@ -6,8 +6,10 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
-   Product.find()
+  Product.find()
   .then(products => {
     console.log(products);
      res.render('shop/product-list', {
@@ -41,7 +43,10 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = req.query.page;
   Product.find()
+  .skip((page - 1) * ITEMS_PER_PAGE)
+  .limit(ITEMS_PER_PAGE)
   .then(product => {
      res.render('shop/index', {
         prods: product,
