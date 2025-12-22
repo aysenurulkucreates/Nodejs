b6a840523571e5248ef8a832a7a6d6dd5cc2e247
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-22 10:25:58 +0300

    Re-Using the Pagination Logic & Controls

--- SessionsCookies/7_addingPagination/controllers/shop.js
+++ SessionsCookies/7_addingPagination/controllers/shop.js
@@ -6,16 +6,32 @@ const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const ITEMS_PER_PAGE = 1;
+const ITEMS_PER_PAGE = 3;
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
-  .then(products => {
-    console.log(products);
+  const page = +req.query.page || 1;
+  let totalItems;
+  
+  Product
+  .find()
+  .countDocuments()
+  .then(numProducts => {
+    totalItems = numProducts;
+    return Product.find()
+      .skip((page - 1) * ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE);
+  })
+.then(product => {
      res.render('shop/product-list', {
-        prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
+        prods: product,
+        pageTitle: 'Products',
+        path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page -1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
   });
   })
   .catch(err => {
