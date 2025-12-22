45cb13a8abf269b633251b34546355b9b3eeb155
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-22 10:28:07 +0300

    CREATİNG NEW FOLDER FOR THE NEW SUBJECT

---
+++ SessionsCookies/8_understandingAsyncRequest/routes/admin.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+const express = require('express');
+const { body } = require('express-validator');
+
+const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
+
+const router = express.Router();
+
+router.get('/add-product', isAuth, adminController.getAddProduct);
+
+router.get('/products', isAuth, adminController.getProducts);
+
+router.post('/add-product',
+    [
+     body('title')
+        .isString()
+        .isLength({ min: 3 })
+        .trim(),
+     body('price')
+        .isFloat(),
+     body('description')
+        .isLength({ min: 8, max: 150 })
+        .trim()
+
+    ],
+    isAuth,
+    adminController.postAddProduct); 
+
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
+
+router.post('/edit-product',
+    [
+     body('title')
+        .isString()
+        .isLength({ min: 3 })
+        .trim(),
+     body('price')
+        .isFloat(),
+     body('description')
+        .trim()
+        .isLength({ min: 8, max: 150 })
+    ],
+     isAuth, 
+     adminController.postEditProduct);
+
+router.post('/delete-product', isAuth, adminController.postDeleteProduct);
+
+module.exports = router;
