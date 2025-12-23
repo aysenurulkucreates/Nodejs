926aa300fb93f82002e4d70bb2492dc87ef5dca1
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-23 19:20:38 +0300

    Sending Requests & Responses and Working with Postman

--- RESTAPI/1_RESTAPIBasics/app.js
+++ RESTAPI/1_RESTAPIBasics/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+app.use(bodyParser.json());   // buna alternatif app.use(bodyParser.urlencoded());
 
 app.use('/feed', feedRoutes);
 
