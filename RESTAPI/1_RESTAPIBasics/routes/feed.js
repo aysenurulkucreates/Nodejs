e762fefadd2b8c5df73dd425d0fb6a16a0045d5f
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-26 15:19:11 +0300

    Updating posts

--- RESTAPI/1_RESTAPIBasics/routes/feed.js
+++ RESTAPI/1_RESTAPIBasics/routes/feed.js
@@ -7,7 +7,8 @@ const router = express.Router();
 
 router.get('/posts', feedController.getPosts);
 
-router.post('/post', [
+router.post('/post', 
+[
     body('title')
      .trim()
      .isLength({ min: 5 }),
@@ -15,6 +16,21 @@ router.post('/post', [
      .trim()
      .isLength({ min: 5 })
 ], 
-feedController.createPost);
+feedController.createPost
+);
+
+router.get('/post/:postId', feedController.getPost);
+
+router.put('/post/:postId', 
+[
+    body('title')
+     .trim()
+     .isLength({ min: 5 }),
+    body('content')
+     .trim()
+     .isLength({ min: 5 })
+], 
+feedController.updatePost
+);
 
 module.exports = router;
