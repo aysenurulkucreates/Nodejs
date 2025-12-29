e5028054fa7aaec02e90e0ac6346dee3c5acdd3b
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-29 18:48:40 +0300

    Sharing the IO instance across files

--- RESTAPI/2_webSockets/app.js
+++ RESTAPI/2_webSockets/app.js
@@ -62,7 +62,7 @@ mongoose
 .connect(process.env.MONGO_URI)
 .then(result => {
   const server = app.listen(8080);
-  const io = require('socket.io')(server);
+  const io = require('./socket').init(server);
   io.on('connection', socket => {
     console.log('Client connected');
   });
