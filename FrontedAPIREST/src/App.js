9f040ff4ad026a9040eec72986c31c44e5b638ac
aysenurulkucreates <aysenurulku17@gmail.com>
2025-12-30 18:48:21 +0300

    Connecting the Frontend to the GraphQL API

--- FrontedAPIREST/src/App.js
+++ FrontedAPIREST/src/App.js
@@ -109,44 +109,56 @@ class App extends Component {
   signupHandler = (event, authData) => {
     event.preventDefault();
     this.setState({ authLoading: true });
-    fetch('http://localhost:8080/auth/signup', {
-      method: 'PUT',
+    
+    // Düzeltme 1: q harfi g yapıldı.
+    const graphqlQuery = {
+      query: `
+        mutation {
+          createUser(userInput: {
+            email: "${authData.signupForm.email.value}",
+            name: "${authData.signupForm.name.value}",  
+            password: "${authData.signupForm.password.value}"
+          }) {
+            _id
+            email
+          }
+        }
+      `
+    };
+
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        email: authData.signupForm.email.value,
-        password: authData.signupForm.password.value,
-        name: authData.signupForm.name.value
-      })
+      body: JSON.stringify(graphqlQuery)
+    }) // Düzeltme 2: fetch burada kapatıldı!
+    .then(res => {
+      return res.json();
     })
-      .then(res => {
-        if (res.status === 422) {
-          throw new Error(
-            "Validation failed. Make sure the email address isn't used yet!"
-          );
-        }
-        if (res.status !== 200 && res.status !== 201) {
-          console.log('Error!');
-          throw new Error('Creating a user failed!');
-        }
-        return res.json();
-      })
-      .then(resData => {
-        console.log(resData);
-        this.setState({ isAuth: false, authLoading: false });
-        this.props.history.replace('/');
-      })
-      .catch(err => {
-        console.log(err);
-        this.setState({
-          isAuth: false,
-          authLoading: false,
-          error: err
-        });
+    .then(resData => {
+      if (resData.errors && resData.errors[0].status === 422) {
+        throw new Error(
+          "Validation failed. Make sure the email address isn't used yet!"
+        );
+      }
+      if (resData.errors) {
+        throw new Error("User creation failed!");
+      }
+      
+      console.log(resData);
+      this.setState({ isAuth: false, authLoading: false });
+      this.props.history.replace('/');
+    })
+    .catch(err => {
+      console.log(err);
+      this.setState({
+        isAuth: false,
+        authLoading: false,
+        error: err
       });
-  };
-
+    });
+};
   setAutoLogout = milliseconds => {
     setTimeout(() => {
       this.logoutHandler();
--- GraphQl/1_introduction/app.js
+++ GraphQl/1_introduction/app.js
@@ -46,6 +46,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE, PATCH');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
 
