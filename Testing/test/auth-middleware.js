2b47bff4ecc03ab4d8e915450c65e9c62d225cf9
aysenurulkucreates <aysenurulku17@gmail.com>
2026-01-05 11:33:14 +0300

    Testing controllers

--- Testing/test/auth-middleware.js
+++ Testing/test/auth-middleware.js
@@ -41,7 +41,6 @@ describe('Auth middleware', function() {
     sinon.stub(jwt, 'verify');
     jwt.verify.returns({userId: 'abc'});
     authMiddleware(req, {}, () => {});
-    expect(req).to.have.property('userId');
     expect(req).to.have.property('userId', 'abc');
     expect(jwt.verify.called).to.be.true;
     jwt.verify.restore();
