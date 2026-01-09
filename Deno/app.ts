24f257c6ac99ea92e8553f116e7b182634c205ec
aysenurulkucreates <aysenurulku17@gmail.com>
2026-01-09 10:53:23 +0300

    Repeating the example with node

--- Deno/app.ts
+++ Deno/app.ts
@@ -1,6 +1,6 @@
 const text = "This is a test - and it should be stored in a file!";
 
-const encoder = new TextEncoder();
+const encoder = new TextEncoder(); // bilgisayarın anlayacağı dile çevirmek için bu şart
 const data = encoder.encode(text);
 
 Deno.writeFile("message.txt", data).then(() => {
