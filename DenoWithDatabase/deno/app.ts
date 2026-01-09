d4bb8fe8aa73676821bedabbcd7f72aecc2fe2ec
aysenurulkucreates <aysenurulku17@gmail.com>
2026-01-09 15:07:20 +0300

    Handling cors errors

--- DenoWithDatabase/deno/app.ts
+++ DenoWithDatabase/deno/app.ts
@@ -5,6 +5,22 @@ import todosRoutes from "./routes/todos.ts";
 const app = new Application();
 
 app.use(async (ctx, next) => {
+  // 1. "Kimler gelebilir?" -> "*" demek "Herkes gelebilir" demektir.
+  ctx.response.headers.set("Access-Control-Allow-Origin", "*");
+
+  // 2. "Hangi işlemleri yapabilirler?" -> GET, POST, PUT, DELETE serbest.
+  ctx.response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE"
+  );
+
+  // 3. "Hangi başlıkları gönderebilirler?" -> Content-Type (JSON yolluyoruz ya, o yüzden lazım)
+  ctx.response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+
+  await next(); // Tamam, şimdi diğer kodlara geçebilirsin.
+});
+
+app.use(async (ctx, next) => {
   console.log("Middleware!");
   await next();
 });
